2071a17559209e520056e78aa670cce291bceae0
Baker Humadi <bhumadi@BHUMADI-MLT.local>
2017-07-30 12:50:58 -0700

    final changes b4 demo

--- ios/getFit/getFit/ViewController.swift
+++ ios/getFit/getFit/ViewController.swift
@@ -14,8 +14,7 @@ import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    let locationManager = CLLocationManager()
-    let tabBar = UITabBar()
+//    let locationManager = CLLocationManager()
     
     //locationManager.requestLocation()
 
@@ -30,14 +29,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.locationManager.requestAlwaysAuthorization()
-        self.locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-//            locationManager.startUpdatingLocation()
-        }
+//        self.locationManager.requestAlwaysAuthorization()
+//        self.locationManager.requestWhenInUseAuthorization()
+//
+//        if CLLocationManager.locationServicesEnabled() {
+//            locationManager.delegate = self
+//            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+////            locationManager.startUpdatingLocation()
+//        }
 
     }
 
@@ -46,15 +45,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        // we should call an ednpoint here
-        self.alert(message: "locations = \(locValue.latitude) \(locValue.longitude)", title: "location")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
-        self.alert(message: "Error while updating location " + error.localizedDescription, title: "error")
-    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+//        // we should call an ednpoint here
+//        self.alert(message: "locations = \(locValue.latitude) \(locValue.longitude)", title: "location")
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+//        self.alert(message: "Error while updating location " + error.localizedDescription, title: "error")
+//    }
 
 }
 

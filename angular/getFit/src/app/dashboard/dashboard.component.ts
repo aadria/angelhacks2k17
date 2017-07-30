d85e0b795ee0afe8d9fbbc59096dff66d501dec2
Baker Humadi <bhumadi@BHUMADI-MLT.local>
2017-07-30 12:03:34 -0700

    home p1

--- angular/getFit/src/app/dashboard/dashboard.component.ts
+++ angular/getFit/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   activites = [{ "username": "jonah", "time": "20 hours", "points": "3000", "id": 1 }, { "username": "tj", "time": "30 hours", "points": "3300", "id": 2 }, { "username": "amy", "time": "10 hours", "points": "300", "id": 3 }]
-  constructor() { }
+  constructor(private http: Http) {
+
+  }
 
   ngOnInit() {
+    // this.http.get('http://54.212.234.116:3000/api/activities').map(data => {
+    //   console.log(data);
+    //   if (data !== null) {
+    //     this.activites = data.json()._body;
+    //   }
+
+    //   console.log(this.activites);
+    // }).subscribe();
   }
 
 }
